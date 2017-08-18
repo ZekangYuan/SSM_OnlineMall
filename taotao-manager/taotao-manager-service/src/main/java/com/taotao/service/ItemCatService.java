95643d19129afde7db9b1f840580cd06d90ef591
kangsugo <1522847010@qq.com>
2017-08-19 02:23:32 +0800

    商品目录
    实现查看商品目录

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemCatService.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemCatService.java
@@ -2,8 +2,8 @@ package com.taotao.service;
 
 import java.util.List;
 
-import com.taotao.common.pojo.TreeNode;
+import com.taotao.common.pojo.EUTreeNode;
 
 public interface ItemCatService {
-	public List<TreeNode> getItemCatList(long parentId);
+	List<EUTreeNode> getCatList(long parentId);
 }
